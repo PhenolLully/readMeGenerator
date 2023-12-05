811c30a4e24628656fbad2cf22649f209ec2b85e
Phenol Lully <lullyp95@gmail.com>
2023-12-05 17:34:13 -0500

    Added started code

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+// Node v10+ includes a promises module as an alternative to using callbacks with file system methods.
+const { writeFile } = require('fs').promises;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'motivation',
+      message: 'What was your motivation?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateHTML = ({ name, location, github, linkedin }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="p-5 mb-4 header bg-light">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${name}</h1>
+      <p class="lead">I am from ${location}.</p>
+      <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${github}</li>
+        <li class="list-group-item">LinkedIn: ${linkedin}</li>
+      </ul>
+    </div>
+  </header>
+</body>
+</html>`;
 
-// TODO: Create a function to initialize app
-function init() {}
+`# Read Me Generator
 
-// Function call to initialize app
+## Your Task
+
+- What was your motivation?
+${motivation}
+
+## Installation
+
+
+## Usage
+
+## Credits
+
+## User Story
+
+
+
+## Acceptance Criteria
+`
+
+
+
+// Bonus using writeFileSync as a promise
+const init = () => {
+  promptUser()
+    // Use writeFile method imported from fs.promises to use promises instead of
+    // a callback function
+    .then((answers) => writeFile('index.html', generateHTML(answers)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
+};
+
 init();
+
+
+
